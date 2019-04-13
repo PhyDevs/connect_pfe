4fd1dc96cec0b12bde1084f9c879217e29270aff
phydevs <9onfod71@gmail.com>
2019-04-13 18:51:15 +0100

    Add ConnectUser model configuration

--- Data/ConnectContext.cs
+++ Data/ConnectContext.cs
@@ -19,6 +19,7 @@ namespace Connect.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new ConnectUserConfiguration());
         }
     }
 }
--- Models/ConnectUser.cs
+++ Models/ConnectUser.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Connect.Models
 {
+    public enum Roles
+    {
+        Admin,
+        Teacher,
+        Student
+    }
+
     public class ConnectUser
     {
         public Guid Id { get; set; }
@@ -13,12 +22,18 @@ namespace Connect.Models
 
         public string LastName { get; set; }
 
-        public string FullName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
 
         public int NInscription { get; set; }
 
         public string Password { get; set; }
 
-        public string Role { get; set; }
+        public Roles Role { get; set; }
     }
 }
