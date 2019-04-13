21c6cfa60bb88ded3e1f154e85c4d71137fe5288
phydevs <9onfod71@gmail.com>
2019-04-13 17:57:03 +0100

    add a database context

---
+++ Data/ConnectContext.cs
@@ -0,0 +1,24 @@
+﻿using Connect.Data.EntityConfigurations;
+using Connect.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Connect.Data
+{
+    public class ConnectContext : DbContext
+    {
+        public ConnectContext(DbContextOptions<ConnectContext> options) : base(options) { }
+
+        DbSet<ConnectUser> Users { get; set; }
+        DbSet<Course> Courses { get; set; }
+        DbSet<Department> Departments { get; set; }
+        DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Connect.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Connect
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+    
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ConnectContext>(opt => 
+                opt.UseSqlServer(Configuration.GetConnectionString("ConnectConnectionString"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
