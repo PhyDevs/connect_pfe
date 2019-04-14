a008e4ff663a3bfc69fc5ac4bb6e80f6bb6274a5
phydevs <9onfod71@gmail.com>
2019-04-14 22:52:33 +0100

    add Message repository

---
+++ Data/Repositories/MessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Connect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Connect.Data.Repositories
+{
+    public class MessageRepository : AbstractRepository<Message>
+    {
+        public MessageRepository(ConnectContext context) : base(context) { }
+    }
+}
