40f8f9697301b85e1dfac22dbcdd7b44f8648243
phydevs <9onfod71@gmail.com>
2019-05-10 22:25:14 +0000

    frontend: edit ading validation to InputField component

--- ClientApp/src/components/Form/InputField.jsx
+++ ClientApp/src/components/Form/InputField.jsx
@@ -15,15 +15,16 @@ const Field = styled.div`
 		font-size: 1.12rem;
 		padding: 8px 10px;
 		background-color: transparent;
-		border: 1px solid #cbcbcb;
+		border: 1px solid;
+		border-color: ${props => (props.notValid ? '#ea1000' : '#cbcbcb')};
 		outline: 0;
 		border-radius: 3px;
 		transition: all 0.1s linear;
 		z-index: 2;
 		&:focus {
 			background-color: transparent;
-			border-color: #7300ac;
-			box-shadow: 0 0 2px 1px #7300ac;
+			border-color: ${props => (props.notValid ? '#ea1000' : '#7300ac')};
+			box-shadow: 0 0 2px 1px ${props => (props.notValid ? '#ea1000' : '#7300ac')};
 		}
 
 		& + span {
@@ -38,10 +39,27 @@ const Field = styled.div`
 			transition: all 0.1s linear;
 		}
 	}
+
+	& > span {
+		display: inline-block;
+		margin: 5px 4px 0;
+		color: #ea1000;
+		font-weight: 700;
+		font-size: 0.9rem;
+		letter-spacing: 0.2px;
+	}
 `;
 
-const InputField = ({ label, type, name, width }) => {
+const InputField = ({ label, type, name, pattern, errorMsg, width }) => {
 	const [filled, setFilled] = React.useState(false);
+	const [isValid, setValid] = React.useState(true);
+
+	const ValidateValue = value => {
+		if (pattern === null) return true;
+
+		const regex = new RegExp(pattern);
+		return regex.test(value);
+	};
 
 	const HandelFocus = () => {
 		if (!filled) setFilled(true);
@@ -50,6 +68,9 @@ const InputField = ({ label, type, name, width }) => {
 	const HandelBlur = e => {
 		if (e.target.value.trim() !== '') {
 			setFilled(true);
+
+			if (ValidateValue(e.target.value)) setValid(true);
+			else setValid(false);
 		} else {
 			e.target.value = '';
 			setFilled(false);
@@ -57,11 +78,12 @@ const InputField = ({ label, type, name, width }) => {
 	};
 
 	return (
-		<Field filled={filled} width={width}>
+		<Field filled={filled} notValid={!isValid} width={width}>
 			<label htmlFor={name}>
 				<input type={type} id={name} name={name} onFocus={HandelFocus} onBlur={HandelBlur} />
 				<span>{label}</span>
 			</label>
+			{!isValid && <span>{errorMsg}</span>}
 		</Field>
 	);
 };
@@ -70,11 +92,15 @@ InputField.propTypes = {
 	label: PropTypes.string.isRequired,
 	name: PropTypes.string.isRequired,
 	type: PropTypes.string,
+	pattern: PropTypes.string,
+	errorMsg: PropTypes.string,
 	width: PropTypes.number,
 };
 
 InputField.defaultProps = {
 	type: 'text',
+	pattern: null,
+	errorMsg: 'This value is not valid',
 	width: 100,
 };
 
