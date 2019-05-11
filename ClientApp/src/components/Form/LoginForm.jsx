af3eaf8c9ed8170ad49fbe815bf4d330f2e770f5
phydevs <9onfod71@gmail.com>
2019-05-11 22:33:25 +0000

    frontend: add LoginForm component

---
+++ ClientApp/src/components/Form/LoginForm.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+import colors from '../../utils/colors';
+import InputField from './InputField';
+import Button from '../Common/Button';
+import { LoginContext } from '../../providers/LoginContext';
+
+const Box = styled.div`
+	width: 100%;
+	max-width: 350px;
+	margin: 10px 15px;
+	padding: 40px 35px;
+	background-color: ${colors.light};
+	border-radius: 6px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.12), 0 2px 10px rgba(0, 0, 0, 0.08);
+
+	h1 {
+		margin: 0 4px 40px;
+		padding: 0;
+		text-align: center;
+		font-size: 2rem;
+		font-weight: 700;
+	}
+`;
+
+const BottomText = styled.div`
+	margin: 30px 0 0;
+	text-align: center;
+
+	a {
+		color: inherit;
+		font-weight: 700;
+		text-decoration: none;
+	}
+`;
+
+const LoginForm = ({ title }) => {
+	const [hasErrors] = React.useContext(LoginContext);
+
+	const HandelSubmit = e => {
+		e.preventDefault();
+		if (Object.values(hasErrors).indexOf(true) >= 0) return;
+
+		// Handel Login
+		console.log('Passed');
+	};
+
+	return (
+		<Box>
+			<h1>{title}</h1>
+			<form method="post" style={{ padding: '20px 0' }} onSubmit={HandelSubmit}>
+				<InputField label="Number" name="number" pattern="^\d{5}$" />
+				<InputField label="Password" name="password" type="password" pattern="^.{4,}$" />
+
+				<Button type="submit" value="Sign In" />
+			</form>
+
+			<BottomText>
+				Don&lsquo;t have an account? <Link to="/signup">Sign Up</Link>
+			</BottomText>
+		</Box>
+	);
+};
+
+LoginForm.propTypes = {
+	title: PropTypes.string.isRequired,
+};
+
+export default LoginForm;
